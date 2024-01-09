84808d34b64332db47de35d4269cf89dc4709c15
admin <dev@rhoatech.com>
2024-01-10 06:39:52 +1000

    Added component-label associations, and fixed setting the sizesof components.

--- tests/backend/ProfileTests.java
+++ tests/backend/ProfileTests.java
@@ -51,7 +51,8 @@ public class ProfileTests
     @Test
     public void deleteTestProfileTest ()
     {
-        URProfilesUtil.createProfile(testDriver.getTestProfileName());
+        if(!URProfilesUtil.profileExists(testDriver.getTestProfileName()))
+            URProfilesUtil.createProfile(testDriver.getTestProfileName());
         URProfilesUtil.setActiveProfileName(testDriver.getTestProfileName());
         assertTrue(URProfilesUtil.getActiveProfileName().equals(testDriver.getTestProfileName()));
         // Delete the active profile
